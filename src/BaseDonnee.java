90719fb0b14d66ceac872a493f4383defd9ccbc5
Unknown <marcandreditlebreton@hotmail.com>
2020-01-10 14:28:39 -0500

    Implementation de la methode d'ecriture des utilisateurs sur disque

--- src/BaseDonnee.java
+++ src/BaseDonnee.java
@@ -1,9 +1,11 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
+import java.io.File;
 
 public class BaseDonnee {
 	
@@ -13,7 +15,7 @@ public class BaseDonnee {
  * OUTPUT : LinkedHashMap contenant l'information du fichier
  * */
 	public Map<String,String> readFromFile(String fileLoc) {
-		Map<String, String> users = new LinkedHashMap<String, String>();
+		Map<String, String> users = new HashMap<String, String>();
 		try {
 			BufferedReader in = new BufferedReader(new FileReader(fileLoc));
 
@@ -37,5 +39,30 @@ public class BaseDonnee {
 			e.printStackTrace();
 		}
 		return users;
+	}
+	
+	public void writeToFile(HashMap<String,String> users, String fileLoc) {
+		try {
+            File file = new File(fileLoc);
+
+            
+            file.createNewFile();
+            // On ajoute les users depuis le debut du fichier, donc on overwrite? TODO: Verifier
+            FileWriter fw = new FileWriter(fileLoc,false);
+         
+        	
+        	for (String i : users.keySet()) {
+        		 fw.write( i + " " + users.get(i));
+        		 fw.write(System.lineSeparator());
+        		}
+        	
+        	fw.close();
+        	
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		
+ 
+		
 	}
 }
