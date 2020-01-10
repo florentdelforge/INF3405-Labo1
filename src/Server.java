a9591a30155231cf0c0737b63dfe0dcaf567c564
Florent Delforge <florentdelforge@hotmail.com>
2020-01-09 19:32:09 -0500

    Ajout 2e partie

--- src/Server.java
+++ src/Server.java
@@ -24,3 +24,52 @@ public class Server {
 	 
  }
 }
+
+/*
+ * Une thread qui se charge de traiter la demande de chaque client sur un socket particulier
+*/
+
+private static class ClientHandler extends Thread
+{
+	private Socket socket;
+	private int clientNumber;
+	
+	public ClientHandler(Socket socket, int clientNumber)
+	{
+		this.socket = socket; 
+		this.clientNumber = clientNumber;
+		System.out.println("New connection with client#"+clientNumber+"at"+socket);
+	}
+
+	/*
+	 * Une thread se charge d'envoyer au client un message de bienvenue
+	*/
+	public void run()
+	{
+		try
+		{
+			// Création d'un canal sortant pour e nvoyer des messages au client
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			
+			// Envoi d'un message au client
+			out.writeUTF("Hello from server - you are client#"+clientNumber);
+			
+		} catch (IOException e)
+		{
+			System.out.println("Error handling client#"+clientNumber+":"+e);
+		}
+		finally
+		{
+			try
+			{
+				// Fermeture de la connexion avec le client
+				socket.close();
+			}
+			catch (IOException e)
+			{
+				System.out.println("Couldn't close a socket, what's going on?");
+			}
+			System.out.println("Connection with client#"+clientNumber+"closed");
+		}
+	}
+}
